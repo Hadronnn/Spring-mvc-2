0d13da3cda1afe65261815da5820a6e13bbb0b3c
hadronnn <1364636420@qq.com>
2019-07-09 10:29:30 +0800

    在需要处理异常的Controller中添加异常的处理方法

--- src/com/oaec/springmvc/controller/HelloController.java
+++ src/com/oaec/springmvc/controller/HelloController.java
@@ -2,20 +2,35 @@ package com.oaec.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping("textException")
-    public String testException(Integer num, Model model){
-        try {
+    @RequestMapping("test1")
+    public String test1(Integer num, Model model){
+//        try {
             int result = 10 / num;
             model.addAttribute("result",result);
             return "hello";
-        }catch (Exception e){
-            model.addAttribute("e",e);
-            return "error/500";
-        }
+//        }catch (Exception e){
+//            model.addAttribute("e",e);
+//            return "error/500";
+//        }
     }
-
+    @RequestMapping("test2")
+    public String test2(String name,Model model){
+        int length = name.length();
+        model.addAttribute("result",length);
+        return "hello";
+    }
+    //统一异常处理
+    @ExceptionHandler
+    public ModelAndView exception(Exception e){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error/500");
+        modelAndView.addObject("e",e);
+        return modelAndView;
+    }
 }
